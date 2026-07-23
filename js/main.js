700c0721295c67d1457dde23bd0532a90413703f
JuraevStudio <53979293+Otabek777@users.noreply.github.com>
2026-07-23 16:14:15 +0500

    lenis

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-$(document).ready(function() {
-    $(".questions__block").hide();
-
-    $(".questions__btn").click(function() {
-        $(this).toggleClass('open');
-        $(this).next(".questions__block").slideToggle(300);
-    });
-});
+const lenis=new Lenis({wrapper:document.querySelector('.scroll-container'),lerp:0.1,duration:1.2,smoothTouch:true});lenis.on('scroll',e=>{console.log(e)});function raf(time){lenis.raf(time);requestAnimationFrame(raf)}requestAnimationFrame(raf);const animatedElements=document.querySelectorAll('.animate-on-scroll');const observerOptions={root:null,rootMargin:'0px',threshold:0.1};const observer=new IntersectionObserver((entries,observer)=>{entries.forEach(entry=>{if(entry.isIntersecting){entry.target.classList.add('is-visible');observer.unobserve(entry.target)}else{entry.target.classList.remove('is-visible')}})});animatedElements.forEach(el=>observer.observe(el));$(document).ready(function(){$(".questions__block").hide();$(".questions__btn").click(function(){$(this).toggleClass('open');$(this).next(".questions__block").slideToggle(300)})});
